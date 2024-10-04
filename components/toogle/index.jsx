8b375e28c36a325570fc5e3f5f3f99287e2bff81
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-04 15:04:46 +0300

    düzenleme yapıldı

--- components/toogle/index.jsx
+++ components/toogle/index.jsx
@@ -4,7 +4,10 @@ export default function Toggle({ handleToggle, isChecked }) {
     <div className="toggle">
       <img src="./img/sunny.svg" />
       <label className="switch">
-        <input type="checkbox" onChange={handleToggle} checked={isChecked} />
+        <input type="checkbox" onChange={handleToggle} checked={isChecked == "dark" ? true : false} /> 
+        { /* Eğer temamız dark ise checked'ı true yapacak. */ }
+        { /* Eğer temamız light ise checked'ı false yapacak. */ }
+        {/* Eski halinde isChecked string geliyordu, şimdi sorgu aracılığıyla boolean olan true ve false u getirdik. */}
         <span className="slider round"></span>
       </label>
       <img src="./img/moon.svg" />
